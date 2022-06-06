1bd16d5a3bc08921b1c8cc0b73c6f34f03e7c165
1y38a6 <maximusglabar@gmail.com>
2022-06-06 22:08:36 +0300

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,119 +1,144 @@
-var coef = 3.5;
-
-function shortChanged()
-{
-    var short = parseFloat($("#short1").val());
-    var atr = parseFloat($("#short2").val());
-    $("#short3").val(parseFloat((atr*0.2)+short));
-    $("#short4").val(parseFloat(short-(atr*0.2*coef)));
-}
-
-function longChanged()
-{
-    var long = parseFloat($("#long1").val());
-    var atr = parseFloat($("#long2").val());
-    $("#long3").val(parseFloat(long-(atr*0.2)));
-    $("#long4").val(parseFloat(long+(atr*0.2*coef)));
-}
-
-function coefChanged()
-{
-    coef = parseFloat($("#coef").val());
-    longChanged();
-    shortChanged();
-}
+//#region ST & TP
 
 function longChangedNew()
 {
-    var price = parseFloat($("#longPrice").val());
-    var plusPrice = price * parseFloat($("#longAddPricePercent").val())/100;
+    var price = parseFloat( $( "#longPrice" ).val() );
+    var plusPrice = PlusPrice( price, false );
     price += plusPrice;
-    var stop = parseFloat($("#longStopPrecent").val());
-    var take = parseFloat($("#longTakePercent").val());
+    var stop = parseFloat( $( "#longStopPrecent" ).val() );
+    var take = parseFloat( $( "#longTakePercent").val() );
 
-    localStorage.setItem("longStopPrecent", stop);
-    localStorage.setItem("longTakePercent", take);
+    localStorage.setItem( "longStopPrecent", stop );
+    localStorage.setItem( "longTakePercent", take );
     
-    var st = price + (price * stop/100);
-    var tp = price + (price * take/100);
+    var st = Take( price, stop );
+    var tp = Take( price, take );
 
-    $("#longST").val(price + (price * stop/100));
-    $("#longTP").val(tp);
-    $("#longPP").val(price);
-    
-    price = Math.round( price * 100000 ) / 100000;
-    st = Math.round( st * 100000 ) / 100000;
-    tp = Math.round( tp * 100000 ) / 100000;
+    $( "#longST" ).val( st );
+    $( "#longTP" ).val( tp );
+    $( "#longPP" ).val( price );
 
-    var priceString = price.toString().replace( ".", "," );
-    var stString = st.toString().replace( ".", "," );
-    var tpString = tp.toString().replace( ".", "," );
-
-    var signal = "⬆️LONG⬆️<br><br>" + $("#longCoin").val() + "<br>Stop-Market " + priceString + "<br>TP " + tpString + "<br>SL " + stString;
+    var signal = Signal( "⬆️LONG⬆️", $( "#longCoin" ).val(), price, tp, st );
     $("#longSignal").html( signal );
 }
 
 function shortChangedNew()
 {
-    var price = parseFloat($("#shortPrice").val());
-    var plusPrice = price * parseFloat($("#shortAddPricePercent").val())/100;
+    var price = parseFloat( $( "#shortPrice" ).val() );
+    var plusPrice = PlusPrice( price, true );
     price += plusPrice;
-    var stop = parseFloat($("#shortStopPrecent").val());
-    var take = parseFloat($("#shortTakePercent").val());
+    var stop = parseFloat( $( "#shortStopPrecent" ).val() );
+    var take = parseFloat( $( "#shortTakePercent").val() );
 
-    localStorage.setItem("shortStopPrecent", stop);
-    localStorage.setItem("shortTakePercent", take);
+    localStorage.setItem( "shortStopPrecent", stop );
+    localStorage.setItem( "shortTakePercent", take );
 
-    var st = price + (price * stop/100);
-    var tp = price + (price * take/100);
+    var st = Take( price, stop );
+    var tp = Take( price, take );
 
-    $("#shortST").val(st);
-    $("#shortTP").val(tp);
-    $("#shortPP").val(price);
+    $( "#shortST" ).val( st );
+    $( "#shortTP" ).val( tp );
+    $( "#shortPP" ).val( price );
+
+    var signal = Signal( "⬇️SHORT⬇️", $( "#shortCoin" ).val(), price, tp, st );
+    $( "#shortSignal" ).html( signal );
+}
+
+function Take( price, take )
+{
+    return price + ( price * take / 100 );
+}
+
+function PlusPrice( price, isShort )
+{
+    if( isShort )
+        return price * parseFloat( $( "#shortAddPricePercent" ).val() ) / 100;
+    else
+        return price * parseFloat( $( "#longAddPricePercent" ).val() ) / 100;
+}
+
+function Signal( name, coin, price, tp, st )
+{
+    roundedPrice = Round( price );
+    roundedSt = Round( st );
+    roundedTp = Round( tp );
+
+    var priceString = ReplaceDot( roundedPrice );
+    var stString = ReplaceDot( roundedSt );
+    var tpString = ReplaceDot( roundedTp );
+    return signal = name + "<br><br>" + coin + "<br>Stop-Market " + priceString + "<br>TP " + tpString + "<br>SL " + stString;
+}
+
+function Round( num )
+{
+    return Math.round( num * 100000 ) / 100000;
+}
+
+function ReplaceDot( str )
+{
+    return str.toString().replace( ".", "," );
+}
+
+//#endregion
+
+//#region ATR
+
+function PercentChanged()
+{
+    var atr = parseFloat( $( "#atr1" ).val() );;
+    var price = parseFloat( $( "#price1" ).val() );;
     
-    price = Math.round( price * 100000 ) / 100000;
-    st = Math.round( st * 100000 ) / 100000;
-    tp = Math.round( tp * 100000 ) / 100000;
-    
-    var priceString = price.toString().replace( ".", "," );
-    var stString = st.toString().replace( ".", "," );
-    var tpString = tp.toString().replace( ".", "," );
-
-    var signal = "⬇️SHORT⬇️<br><br>" + $("#shortCoin").val() + "<br>Stop-Market " + priceString + "<br>TP " + tpString + "<br>SL " + stString;
-    $("#shortSignal").html( signal );
+    var firstOrder = atr / 100 * 30;
+    var betweenOrder = atr / 100 * 20;
+    var longFirstOrderCost = price - price * ( firstOrder / 100 );
+    $( "#long1" ).val( longFirstOrderCost );
+    for( var i = 2; i < 6; i++ )
+    {
+        longFirstOrderCost -= longFirstOrderCost * ( betweenOrder / 100 );
+        $( "#long" + i ).val( longFirstOrderCost );
+    }
+    var shortFirstOrderCost = price + price * ( firstOrder / 100 );
+    $( "#short1" ).val( shortFirstOrderCost );
+    for( var i = 2; i < 6; i++ )
+    {
+        shortFirstOrderCost += shortFirstOrderCost * ( betweenOrder / 100 );
+        $( "#short" + i ).val( shortFirstOrderCost );
+    }
 }
 
 function riskChanged()
 {
-    var money = parseFloat($("#risk1").val());
-    var inprice = parseFloat($("#risk2").val());
-    var out = parseFloat($("#risk3").val());
-    var count = parseFloat(money/(inprice-out));
-    $("#risk4").val(count);
-    $("#risk5").val(count*inprice);
+    var money = parseFloat( $( "#risk1" ).val() );
+    var inprice = parseFloat( $( "#risk2").val() );
+    var out = parseFloat( $( "#risk3" ).val() );
+    var count = parseFloat( money / ( inprice-out ) );
+    $( "#risk4" ).val( count );
+    $( "#risk5" ).val( count*inprice );
 }
 
-function PercentChanged()
+var coef = 3.5;
+
+function shortChanged()
 {
-    var atr = parseFloat($("#atr1").val());;
-    var price = parseFloat($("#price1").val());;
-    
-    var firstOrder = atr/100*30;
-    // $("#first").val(firstOrder + "%");
-    var betweenOrder = atr/100*20;
-    // $("#between").val(betweenOrder + "%");
-    var longFirstOrderCost = price-price*(firstOrder/100);
-    $("#long1").val(longFirstOrderCost);
-    for(var i = 2; i<6; i++)
-    {
-        longFirstOrderCost-=longFirstOrderCost*(betweenOrder/100);
-        $("#long" + i).val(longFirstOrderCost);
-    }
-    var shortFirstOrderCost = price+price*(firstOrder/100);
-    $("#short1").val(shortFirstOrderCost);
-    for(var i = 2; i<6; i++)
-    {
-        shortFirstOrderCost+=shortFirstOrderCost*(betweenOrder/100);
-        $("#short"+i).val(shortFirstOrderCost);
-    }
+    var short = parseFloat( $( "#short1" ).val() );
+    var atr = parseFloat( $( "#short2" ).val() );
+    $( "#short3" ).val( parseFloat( ( atr * 0.2 ) + short ) );
+    $( "#short4" ).val( parseFloat( short - ( atr * 0.2 * coef ) ) );
 }
+
+function longChanged()
+{
+    var long = parseFloat( $( "#long1" ).val() );
+    var atr = parseFloat( $( "#long2" ).val() );
+    $( "#long3" ).val(parseFloat( long - ( atr * 0.2 ) ) );
+    $( "#long4" ).val(parseFloat( long + ( atr * 0.2 * coef ) ) );
+}
+
+function coefChanged()
+{
+    coef = parseFloat( $( "#coef" ).val() );
+    longChanged();
+    shortChanged();
+}
+
+//#endregion
